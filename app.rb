8ce07db2312920f3c9daf6d27dc168d7d9f41369
Alexy <alexandra.fina@yahoo.com>
2018-01-17 14:43:38 +0000

    Entering players - get and post - show the names on screen

--- app.rb
+++ app.rb
@@ -2,12 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    # 'Testing infrastructure working!'
+    erb :index
   end
-  # get '/' do
-  #   name = %w[Ben Alex Agata Tom].sample
-  #   "Hello #{name}"
-  # end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,6 +1,14 @@
 feature '#Homepage' do
-  scenario 'Testing Home Page Content' do
+  # scenario 'Testing Home Page Content' do
+  #   visit '/'
+  #   expect(page).to have_content 'Testing infrastructure working!'
+  # end
+
+  scenario 'should expect players to fill in names' do
     visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in "player_1_name", with: "Benjamin"
+    fill_in "player_2_name", with: "Alexandra"
+    click_button "Submit"
+    expect(page).to have_text("Benjamin vs. Alexandra")
   end
 end
