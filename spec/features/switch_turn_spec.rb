b7f27ae1927ba813085f1255c442924080b8c59e
philb56 <phil.bugden@outlook.com>
2018-01-18 18:01:07 +0000

    Switching turns

--- spec/features/switch_turn_spec.rb
+++ spec/features/switch_turn_spec.rb
@@ -2,7 +2,7 @@ feature '#switch_turn' do
   scenario 'Should display player2 turn' do
     sign_in_and_play
     click_button 'Attack'
-    #click_button 'Ok'
+    click_button 'Ok'
     expect(page).to have_content("Alex's turn")
   end
 end
