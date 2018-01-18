2281b0b89ed0efac68ae6821580bda97ba292884
philb56 <phil.bugden@outlook.com>
2018-01-18 15:34:04 +0000

    Skinny Controllers

--- app.rb
+++ app.rb
@@ -11,33 +11,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    # session[:player_1_name] = params[:player_1_name]
-    # session[:player_2_name] = params[:player_2_name]
-
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1,player_2)
     redirect '/play'
   end
 
   get '/play' do
-    # @player_1_name = session[:player_1_name]
-    # @player_2_name = session[:player_2_name]
-
-    @player_1_name = $player_1.player_name
-    @player_2_name = $player_2.player_name
-
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    # @player_1_name = session[:player_1_name]
-    # @player_2_name = session[:player_2_name]
-    
-    @player_1_name = $player_1.player_name
-    @player_2_name = $player_2.player_name
-    @player_2_score = Game.new.attack($player_2)
-
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,10 @@
 require 'game'
 
 describe Game do
-  subject(:game)          {described_class.new}
   let(:player_1)          { double :player_1, new: name , receive_damage: 90}
   let(:player_2)          { double :player_2 }
   let(:name)              { 'Ben' }
-
+  subject(:game)          {described_class.new(player_1,player_2)}
 
   it "Should attack player " do
     #player_1 = Player.new('Ben')
@@ -16,5 +15,16 @@ describe Game do
   it 'damages the player' do
     expect(player_2).to receive(:receive_damage)
     game.attack(player_2)
+  end
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq player_2
+    end
   end
 end
